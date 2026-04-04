8c6d51e464038d5cc71299600561b47189abbb79
krishankanhaiya09 <kanhaiya057@gmail.com>
2026-04-04 08:44:43 +0530

    removed real ai chat and added dummy chatbot

--- routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authMiddleware from "../middleware/authMiddleware.js";
-import { askHealthAssistant } from "../controllers/chatController.js";
-
-const router = express.Router();
-
-router.post("/", authMiddleware, askHealthAssistant);
-
-export default router;
